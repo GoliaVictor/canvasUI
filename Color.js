3c06397bfc3e59cdad5da7101c98ec98a6aa9f30
GoliaVictor <62819330+GoliaVictor@users.noreply.github.com>
2023-01-13 03:19:33 +1100

    added hsbToRgb function

--- Color.js
+++ Color.js
@@ -49,4 +49,12 @@ let Color = {
             Color.nearInverse = Color.nearWhite
         }
     },
+
+    hsbToRgb(h, s, b) {
+        s /= 100;
+        b /= 100;
+        const k = (n) => (n + h / 60) % 6;
+        const f = (n) => b * (1 - s * Math.max(0, Math.min(k(n), 4 - k(n), 1)));
+        return [Math.round(255 * f(5)), Math.round(255 * f(3)), Math.round(255 * f(1))];
+    }
 }
